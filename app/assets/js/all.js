340f7784333b6eb324921b1d970cbb8feb03e715
Vic <Vic@gmail.com>
2020-06-02 13:16:09 +0800

    fix:swiper斷點

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,7 +4,6 @@ $(document).ready(() => {
   var swiper__inspiration = new Swiper('.section__inspiration .swiper-container', {
     slidesPerView: 3,
     spaceBetween: 30,
-    slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
@@ -15,89 +14,42 @@ $(document).ready(() => {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
-  $(window).resize(function () {
-    if($(window).width()<=576){
-      var swiper__Choices = new Swiper('.section__topChoices .swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-      var swiper__inspiration = new Swiper('.section__inspiration .swiper-container', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-    }else if($(window).width()<=996){
-      var swiper__Choices = new Swiper('.section__topChoices .swiper-container', {
+    breakpoints: {
+      576: {
         slidesPerView: 2,
-        spaceBetween: 30,
-        slidesPerGroup: 2,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-      
-    
-    }else if($(window).width()<=1200){
-      var swiper__Choices = new Swiper('.section__topChoices .swiper-container', {
+      },
+      768: {
         slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-    }else{ var swiper__Choices = new Swiper('.section__topChoices .swiper-container', {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      slidesPerGroup: 4,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+      
     }
   });
+  var swiper__Choices = new Swiper('.section__topChoices .swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+      },
+      1199: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      
+    }
+  });
+      
   
 });
